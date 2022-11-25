aa08ae9da92b512e3e30e1b6e71b76dd92b856e0
Raj Karmakar <97rajmath@gmail.com>
2022-11-25 17:07:10 +0530

    Updated formatting & other minor changes

--- models/Catalog.js
+++ models/Catalog.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const {Schema,model}=require('mongoose');
 
 let catalog = {
-    catalog_id   :  {type:String, required:false, unique:true},
-    products :  {type:Array, required:true, default: []},
+    catalog_id: {type:String, required:false, unique:true},
+    products: {type:Array, required:true, default: []},
 	seller_id: {type: String, required: true},
-    created_on  :  {type:Date, required:false, default:Date.now},
+    created_on: {type:Date, required:false, default:Date.now},
 };
 
 let Catalog = model('catalogs', new Schema(catalog));
