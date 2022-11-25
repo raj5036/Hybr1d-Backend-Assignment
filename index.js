2246cc025ab795339e1e3d76a3e4376147cfcb28
Raj Karmakar <97rajmath@gmail.com>
2022-11-25 10:54:02 +0530

    Implemented basic mongoose connection setup

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Connect to db
+mongoose.connect(
+	'mongodb://localhost:27017/twitterDB',
+	{
+		useNewUrlParser:true,
+		useUnifiedTopology:true,
+	}
+);
+
+//Check connection
+const db = mongoose.connection;
+db.once('open', () => {
+	console.log("Connected to DB");
+});
 
 app.get("/", (req, res) => {
 	res.send("Ok");
